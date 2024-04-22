ad35d6f347913d738266602102e2ea24cff91442
kirkol94 <kirkol94@ya.ru>
2024-04-22 14:16:45 +0500

    plans block

--- src/pages/Home/ui/Home.tsx
+++ src/pages/Home/ui/Home.tsx
@@ -1,12 +1,14 @@
 import { Box } from '@mui/material'
 import { Intro } from './Intro/Intro'
 import { Branches } from './Branches/Branches'
+import { Plans } from './Plans/Plans'
 
 export const Home = () => {
   return (
     <Box>
       <Intro />
       <Branches />
+      <Plans />
     </Box>
   )
 }
---
+++ src/pages/Home/ui/Plans/Plans.tsx
@@ -0,0 +1,17 @@
+import { Box, Container, Typography } from '@mui/material'
+import c from './Plans.module.css'
+
+export const Plans = () => {
+  return (
+    <Box className={c.container}>
+      <Container>
+        <Typography variant="h3" component="h2" align="center">
+          Планы
+        </Typography>
+        <Typography variant="h5" component="p" align="center">
+          В планах в каждый стартовый шаблон добавить скрипт для автогенерации FSD слоёв
+        </Typography>
+      </Container>
+    </Box>
+  )
+}
