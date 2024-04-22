b4ba45cced47b9af18d7ddafd947ba1caf457b55
kirkol94 <kirkol94@ya.ru>
2024-04-22 12:57:10 +0500

    style cardTemplate

--- src/pages/Home/ui/Branches/Branches.tsx
+++ src/pages/Home/ui/Branches/Branches.tsx
@@ -1,6 +1,6 @@
-import { Box, Container, Link, List, ListItem, Typography } from '@mui/material'
+import { CardTemplate } from '@/widgets/CardTemplate'
+import { Box, Container, List, Typography } from '@mui/material'
 import c from './Branches.module.css'
-import DownloadIcon from '@mui/icons-material/Download'
 
 export const Branches = () => {
   return (
@@ -20,56 +20,9 @@ export const Branches = () => {
 
         <Box className={c.listBox}>
           <List className={c.list}>
-            <ListItem className={c.listItem}>
-              <Link
-                underline="none"
-                href="https://github.com/KirKol94/react-start-templates/archive/refs/heads/clear-react-ts.zip"
-                className={c.link}
-              >
-                <DownloadIcon color="primary" />
-                <Typography variant="h5" component="span" className={c.name}>
-                  React TS
-                </Typography>
-              </Link>
-
-              <Typography variant="h5" component="span" className={c.text}>
-                Чистое React TS приложение
-              </Typography>
-            </ListItem>
-
-            <ListItem className={c.listItem}>
-              <Link
-                underline="none"
-                href="https://github.com/KirKol94/react-start-templates/archive/refs/heads/material-ui.zip"
-                className={c.link}
-              >
-                <DownloadIcon color="primary" />
-                <Typography variant="h5" component="span" className={c.name}>
-                  Material UI
-                </Typography>
-              </Link>
-
-              <Typography variant="h5" component="span" className={c.text}>
-                React приложение с подключенной библиотекой Material UI
-              </Typography>
-            </ListItem>
-
-            <ListItem className={c.listItem}>
-              <Link
-                underline="none"
-                href="https://github.com/KirKol94/react-start-templates/archive/refs/heads/tailwindcss.zip"
-                className={c.link}
-              >
-                <DownloadIcon color="primary" />
-                <Typography variant="h5" component="span" className={c.name}>
-                  Tailwindcss
-                </Typography>
-              </Link>
-
-              <Typography variant="h5" component="span" className={c.text}>
-                React приложение с подключенным препроцессором Tailwindсss
-              </Typography>
-            </ListItem>
+            <CardTemplate title="Clear" branchName="clear-react-ts" description="Чистое react + TS" />
+            <CardTemplate title="Tailwindcss" branchName="tailwindcss" description="React + TS + Tailwind" />
+            <CardTemplate title="Material UI" branchName="material-ui" description="React + TS + Material-UI" />
           </List>
         </Box>
       </Container>
---
+++ src/widgets/CardTemplate/ui/CardTemplate.tsx
@@ -0,0 +1,48 @@
+import { Download, GitHub } from '@mui/icons-material'
+import { Box, Link, ListItem, Typography } from '@mui/material'
+import c from './CardTemplate.module.css'
+
+interface Props {
+  title: string
+  branchName: string
+  description: string
+}
+
+export const CardTemplate = (props: Props) => {
+  const { title, branchName, description } = props
+
+  return (
+    <ListItem className={c.listItem}>
+      <Typography variant="h5" component="h3" className={c.title}>
+        {title}
+      </Typography>
+
+      <Typography variant="h5" component="span" className={c.description}>
+        {description}
+      </Typography>
+
+      <Box className={c.links}>
+        <Link
+          className={c.link}
+          href={`https://github.com/KirKol94/react-start-templates/archive/refs/heads/${branchName}.zip`}
+        >
+          <Download />
+          <Typography variant="h5" component="span" className={c.description}>
+            Скачать
+          </Typography>
+        </Link>
+
+        <Link
+          className={c.link}
+          href={`https://github.com/KirKol94/react-start-templates/tree/${branchName}`}
+          target="_blank"
+        >
+          <GitHub />
+          <Typography variant="h5" component="span" className={c.description}>
+            GitHub
+          </Typography>
+        </Link>
+      </Box>
+    </ListItem>
+  )
+}
