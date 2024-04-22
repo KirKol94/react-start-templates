cf82c8a5b9f4a4e6bb3b355a230e0ec68f608102
kirkol94 <kirkol94@ya.ru>
2024-04-22 14:44:57 +0500

    intro bg image

--- src/pages/Home/ui/Intro/Intro.tsx
+++ src/pages/Home/ui/Intro/Intro.tsx
@@ -5,28 +5,30 @@ import c from './Intro.module.css'
 export const Intro = () => {
   return (
     <Box className={c.intro}>
-      <Container className={c.container}>
-        <Typography variant="h3" component="h1">
-          React Start Templates
-        </Typography>
+      <Box className={c.backdrop}>
+        <Container className={c.container}>
+          <Typography variant="h3" component="h1">
+            React Start Templates
+          </Typography>
 
-        <Typography variant="h5" component="p">
-          Начни работу с React с нужным стартовым шаблоном и не трать время на настройку проекта
-        </Typography>
+          <Typography variant="h5" component="p">
+            Начни работу с React с нужным стартовым шаблоном и не трать время на настройку проекта
+          </Typography>
 
-        <Box className={c.buttons}>
-          <Button
-            variant="contained"
-            size="large"
-            target="_blank"
-            LinkComponent="a"
-            href="https://github.com/KirKol94/react-start-templates"
-          >
-            <GitHubIcon />
-            GitHub
-          </Button>
-        </Box>
-      </Container>
+          <Box className={c.buttons}>
+            <Button
+              variant="contained"
+              size="large"
+              target="_blank"
+              LinkComponent="a"
+              href="https://github.com/KirKol94/react-start-templates"
+            >
+              <GitHubIcon />
+              GitHub
+            </Button>
+          </Box>
+        </Container>
+      </Box>
     </Box>
   )
 }
