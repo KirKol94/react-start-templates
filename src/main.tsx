b7bcfac6f18a882a10d69a17537448fc61f17e51
kirkol94 <kirkol94@ya.ru>
2024-04-23 22:26:44 +0500

    add reset.css

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,5 @@
-import ReactDOM from 'react-dom/client'
 import { App } from '@/app/App'
+import '@/app/styles/index.css'
+import ReactDOM from 'react-dom/client'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(<App />)
